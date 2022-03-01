18848c3d3231fc5282eafc45f7632c8f7bd30bcf
bhumi262 <singhbhumi2000@gmail.com>
2022-02-28 22:46:57 -0800

    Update html.py

--- html.py
+++ html.py
@@ -1,1 +1,28 @@
- import pymysql
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .box{
+            border: 10px solid red;
+            width: 50%;
+            height=110;
+        
+
+            padding: 50;
+            border-style:groove;
+            font-style: italic;
+            
+        }
+
+    </style>
+</head>
+<body>
+    <div class ="box" >
+<p colo> ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, cumque harum enim explicabo vero aspernatur, debitis labore hic consequuntur quibusdam perspiciatis quam alias veritatis eligendi numquam maiores quia magni distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quaerat eligendi molestias a dolor enim corporis beatae eius ratione! Sed, dolorem iusto. Similique laudantium sint labore, amet vel dolorum nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque, natus labore ratione cumque ea in nam nulla expedita inventore maxime minus, voluptatem hic modi, ipsum dignissimos porro suscipit! Laborum. consectetur adipisicing elit. Aspernatur facere vero distinctio a soluta libero molestias, atque commodi est sint ratione pariatur obcaecati quod recusandae repellendus perspiciatis minus similique assumenda!</p>
+    </div>
+</body>
+</html>
